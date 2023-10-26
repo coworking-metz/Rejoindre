09566d0b9231ef9e5f3eb921a2964901d57b56f0
Gilles FRANCOIS <gilles@sopress.net>
2023-10-26 11:57:55 +0200

    titre de la page

--- src/router.js
+++ src/router.js
@@ -1,27 +1,40 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/infos",
+      meta: { title: 'Vos informations de contact' },
       component: () => import("@/views/Rejoindre.vue"),
     },
     {
       path: "/",
+      meta: { title: 'Votre rendez-vous' },
       component: () => import("@/views/Visite.vue"),
     },
     {
       path: "/visite",
+      meta: { title: 'Votre rendez-vous' },
       component: () => import("@/views/Visite.vue"),
     },
     {
       path: "/mot-de-passe",
+      meta: { title: 'Votre mot de passe' },
       component: () => import("@/views/MotDePasse.vue"),
     },
     {
       path: "/recap",
+      meta: { title: 'Récapitulatif de votre rendez-vous' },
       component: () => import("@/views/Recap.vue"),
     },
   ],
 });
+// After creating the Vue Router instance
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title + ' - Rejoindre le Coworking Metz';
+  }
+  next();
+});
+export default router;
